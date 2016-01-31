ea33a829a966dea54be6169c934cba0c72e9dc7a
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-01-30 23:29:45 -0600

    go fmt

--- go-sshlogger.go
+++ go-sshlogger.go
@@ -6,44 +6,44 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
-    "os"
-    "time"
+	"os"
+	"time"
 )
 
 /*
-    Copyright 2016 Alexander Scheel <alexander.m.scheel@gmail.com>
-    Licensed under the GPLv3
+   Copyright 2016 Alexander Scheel <alexander.m.scheel@gmail.com>
+   Licensed under the GPLv3
 
-    ~~
+   ~~
 
-    go-sshlog sits and logs incoming ssh attempts on a given port with a given ssh key
+   go-sshlog sits and logs incoming ssh attempts on a given port with a given ssh key
 */
 
 func main() {
-    if (len(os.Args) == 2 && os.Args[1] == "help") || len(os.Args) > 3 {
-        fmt.Println("Usage:", os.Args[0], "[port] [private_key]\nCopyright (C) 2016 Alexander Scheel <alexander.m.scheel@gmail.com>\nLicensed under the GPLv3\n\n\t[port]\t\t- connection string to listen on; default is 0.0.0.0:22\n\t[private_key]\t- private key file for ssh server; default is ./id_rsa\n\nTo generate a new SSH key:\n\tssh-keygen -t rsa -f id_rsa")
-        os.Exit(0)
-    }
+	if (len(os.Args) == 2 && os.Args[1] == "help") || len(os.Args) > 3 {
+		fmt.Println("Usage:", os.Args[0], "[port] [private_key]\nCopyright (C) 2016 Alexander Scheel <alexander.m.scheel@gmail.com>\nLicensed under the GPLv3\n\n\t[port]\t\t- connection string to listen on; default is 0.0.0.0:22\n\t[private_key]\t- private key file for ssh server; default is ./id_rsa\n\nTo generate a new SSH key:\n\tssh-keygen -t rsa -f id_rsa")
+		os.Exit(0)
+	}
 
-    port := "0.0.0.0:22"
-    private_key := "id_rsa"
+	port := "0.0.0.0:22"
+	private_key := "id_rsa"
 
-    for i := range os.Args {
-        if i == 0 {
-            continue
-        }
+	for i := range os.Args {
+		if i == 0 {
+			continue
+		}
 
-        // If it exists, it's  path; otherwise assume it is a port
-        if _, err := os.Stat(os.Args[i]); os.IsNotExist(err) {
-            port = os.Args[i]
-        } else {
-            private_key = os.Args[i]
-        }
-    }
+		// If it exists, it's  path; otherwise assume it is a port
+		if _, err := os.Stat(os.Args[i]); os.IsNotExist(err) {
+			port = os.Args[i]
+		} else {
+			private_key = os.Args[i]
+		}
+	}
 
-	config := &ssh.ServerConfig {
+	config := &ssh.ServerConfig{
 		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-            // Log and deny
+			// Log and deny
 			fmt.Println("time:", time.Now().Format(time.RFC3339), "| from:", c.RemoteAddr().String(), "| user:", string(c.User()), "| pass:", string(pass))
 			return nil, fmt.Errorf("password rejected for %q", c.User())
 		},
